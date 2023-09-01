9c5136ecaf13f11f8f383c4a1aad8cec62f1803d
산유 <San_ew@naver.com>
2023-09-01 23:48:20 +0900

    js 반복문

--- click.js
+++ click.js
@@ -1,3 +1,7 @@
-document.getElementById('box').addEventListener('click', function() {
-  alert('이것은 박스입니다.');
-});
+const box = document.querySelectorAll('#box');
+
+for (let i = 0; i < box.length; i++) {
+  box[i].addEventListener('click', function() {
+    alert(`이것은 ${i + 1}번째 박스입니다.`);
+  });
+}
